e824df5ded6c3985df5fdc43bcf70433adce26f2
Nokorbis <nokorbis@gmail.com>
2022-09-10 11:43:11 +0200

    Initial commit

---
+++ src/main/java/fr/bendertales/mc/talesessentials/TalesEssentials.java
@@ -0,0 +1,13 @@
+package fr.bendertales.mc.talesessentials;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class TalesEssentials implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
